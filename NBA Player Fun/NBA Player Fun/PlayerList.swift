4465f06f83c01fdf3e0419da1ecb142498fca57b
Alexandre C do Carmo <alexandrecdocarmo@Mac-mini-de-Alexandre-2.local>
2022-05-07 15:31:53 -0300

    criado linha e lista para exibir os jogadores

---
+++ NBA Player Fun/NBA Player Fun/PlayerList.swift
@@ -0,0 +1,31 @@
+//
+//  PlayerList.swift
+//  NBA Player Fun
+//
+//  Created by Alexandre C do Carmo on 07/05/22.
+//
+
+import SwiftUI
+
+struct PlayerList: View {
+    var body: some View {
+        
+        NavigationView{
+            List(players){
+                currentPlayer in
+                NavigationLink(destination: ContentView()){
+                    PlayerRow(player: currentPlayer).frame(height: 60)
+                }
+                    
+            }.navigationTitle(Text("NBA Finals PLayers"))
+        }
+        
+        
+    }
+}
+
+struct PlayerList_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerList()
+    }
+}
---
+++ NBA Player Fun/NBA Player Fun/PlayerRow.swift
@@ -0,0 +1,27 @@
+//
+//  PlayerRow.swift
+//  NBA Player Fun
+//
+//  Created by Alexandre C do Carmo on 07/05/22.
+//
+
+import SwiftUI
+
+struct PlayerRow: View {
+    
+    var player: Player
+    
+    var body: some View {
+        HStack {
+            Image(player.imageName).resizable().aspectRatio(contentMode: .fit).clipShape(Circle()).background(Circle().foregroundColor(player.team.color))
+            Text(player.name).font(.largeTitle)
+            Spacer()
+        }
+    }
+}
+
+struct PlayerRow_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerRow(player: players[0]).previewLayout(.fixed(width: 500, height: 100))
+    }
+}
